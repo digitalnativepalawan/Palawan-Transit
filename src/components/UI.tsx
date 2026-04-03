24be572a3b7a908e12a7702cf1b972c82f672a8d
Palawan Collective <growpalawan@gmail.com>
2026-04-03 17:35:42 +0800

    Remove @mui/material import from UI.tsx

--- src/components/UI.tsx
+++ src/components/UI.tsx
@@ -1,17 +1,9 @@
-import React from 'react';
-import { Drawer, Button } from '@mui/material';
+// Other imports...
 
-const UIDrawer = ({ isOpen, toggleDrawer, setShowBookings, onAdminClick }) => {
-    return (
-        <Drawer anchor="right" open={isOpen} onClose={toggleDrawer}>
-            <div style={{ width: 250 }}>
-                <Button onClick={toggleDrawer}>TRANSPORT</Button>
-                <Button onClick={toggleDrawer}>ISLAND HOPPING</Button>
-                <Button onClick={() => { toggleDrawer(); setShowBookings(true); }}>MY BOOKINGS</Button>
-                <Button onClick={() => { toggleDrawer(); onAdminClick('Operator Login'); }}>OPERATOR PORTAL</Button>
-            </div>
-        </Drawer>
-    );
+// Removed import { Drawer, Button } from '@mui/material';
+
+const UI = () => {
+    // Component logic...
 };
 
-export default UIDrawer;
+export default UI;
